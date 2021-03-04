9b875348c19b2db9010397ead2654aef7214f932
lAntraXxXl <79475476+lAntraXxXl@users.noreply.github.com>
2021-03-04 16:33:46 -0600

    Updates

---
+++ precache-manifest.962f0a98fcd90afefe423fd2ef77a1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3c935e69983ad35adc448632059ab3",
+    "url": "/Background_img/index.html"
+  },
+  {
+    "revision": "0aeaeb980d3cc1162910",
+    "url": "/Background_img/static/css/main.e74f6a01.chunk.css"
+  },
+  {
+    "revision": "e913bc01898d7adbef84",
+    "url": "/Background_img/static/js/2.f0671a2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Background_img/static/js/2.f0671a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aeaeb980d3cc1162910",
+    "url": "/Background_img/static/js/main.d1e6b738.chunk.js"
+  },
+  {
+    "revision": "eac03ce497453e6c4a57",
+    "url": "/Background_img/static/js/runtime-main.f2c7efb6.js"
+  }
+]);
